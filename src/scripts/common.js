70589c3b46a2e4dac9399da46f08bfd8c9332828
kasumi0 <parfumdelapeche@gmail.com>
2024-12-13 01:15:22 +0900

    fix/cdn-load-way

--- src/scripts/common.js
+++ src/scripts/common.js
@@ -5,4 +5,10 @@ document.addEventListener('astro:page-load', () => {
   hamburger?.addEventListener("click", () => {
     body.classList.toggle("open");
   });
+
+  const script = document.createElement("script");
+  script.src = "https://kit.fontawesome.com/0a07eaf69b.js";
+  script.defer = true;
+  document.body.appendChild(script);
+
 })
